9d68fc35aa21b3718675f39cce7a9a0005d17a1a
jose <joseluis_1555@hotmail.com>
2022-09-25 17:46:15 -0900

    finalize

--- HandleDetection.py
+++ HandleDetection.py
@@ -3,7 +3,6 @@ import mediapipe as mp
 import time
 import math
 
-
 class poseDetector():
 
     def __init__(self, mode=False, upBody=False, smooth=True,
@@ -17,78 +16,57 @@ class poseDetector():
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
-                                     self.detectionCon, self.trackCon)
+        self.pose = self.mpPose.Pose()
+        # self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
+        #                              self.detectionCon, self.trackCon)
 
     def findPose(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.pose.process(imgRGB)
-        if self.results.pose_landmarks:
-            if draw:
-                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
-                                           self.mpPose.POSE_CONNECTIONS)
-        return img
-
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # inversion de colores
+        self.results = self.pose.process(imgRGB) # devuelve los puntos de referencia de las poses
+        # output : mediapipe.python.solution_base.SolutionOutputs
+        # if self.results.pose_landmarks: # punto de referencia de las poses
+        #     if draw: # dibujo default True
+        #         #Dibuja los puntos de referencia y las conexiones en la imagen.
+        #         self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
+        #                                    self.mpPose.POSE_CONNECTIONS)
+        return imgRGB
+    
     def findPosition(self, img, draw=True):
-        self.lmList = []
+        self.lmList = [] ##   [id,cx,cy]
         if self.results.pose_landmarks:
-            for id, lm in enumerate(self.results.pose_landmarks.landmark):
-                h, w, c = img.shape
-                # print(id, lm)
-                cx, cy = int(lm.x * w), int(lm.y * h)
+            for id, lm in enumerate(self.results.pose_landmarks.landmark): 
+                h, w, c = img.shape # [w,y]
+                cx, cy = int(lm.x * w), int(lm.y * h) #
                 self.lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED) # dibujar las posiciones actuales con puntos
         return self.lmList
 
-    def findAngle(self, img, p1, p2, p3, draw=True):
+    def findAngleAndDrawLandmark(self, img, p1, p2, draw=True):
+        
+        # get coordinates cx,cy for id landmarks 
 
         # Get the landmarks
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        x3, y3 = self.lmList[p3][1:]
-
-        # Calculate the Angle
-        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
-                             math.atan2(y1 - y2, x1 - x2))
-        # print(angle)
-
+        
+        # [id,cx,cy]
+        x1, y1 = self.lmList[p1][1:] # cx1,cy1
+        x2, y2 = self.lmList[p2][1:] # cx2,cy2
         # Draw
         if draw:
             cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
-            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
-            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
-            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
-            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
-            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        return angle
+            cv2.circle(img, (x1, y1), 7, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 7, (0, 0, 255), 2)
 
-def main():
-    cap = cv2.VideoCapture(0)
-    pTime = 0
-    detector = poseDetector()
-    while True:
-        success, img = cap.read()
-        img = detector.findPose(img)
-        lmList = detector.findPosition(img, draw=False)
-        if len(lmList) != 0:
-            print(lmList[14])
-            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+        # 1.distancia entre 2 puntos a mano
+        # 2.distance entre  2 puntos con numpy
+        valuePotentialX = math.pow(x2-x1,2)
+        valuePotentialY = math.pow(y2-y1,2)
+        finalResult = math.sqrt(valuePotentialX + valuePotentialY)
+        return finalResult
+        # # res = False
+        # if(80 <= finalResult): # requried only screen
+        #     # res = True
+        #     cv2.imwrite("images/",img)
+            # cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        # cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 0), 3)
-
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-
-
-if __name__ == "__main__":
-    main()
--- index.py
+++ index.py
@@ -1,14 +1,29 @@
 import numpy as np
 import cv2
 import HandleDetection as pm
+from PIL import ImageGrab
 ################################
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
+video = "video.mp4"
+cap = cv2.VideoCapture(video)
 detector = pm.poseDetector()
+# grab = ImageGrab.grab()
 
 while True:
     success, img = cap.read()
+    img = cv2.resize(img, (500, 400))
     img = detector.findPose(img,False)
+    lmList = detector.findPosition(img, draw=True)
+    if len(lmList) != 0:
+        result = detector.findAngleAndDrawLandmark(img,27,28)
+        # frame = np.array(img)
+        print(result)
+        if(80 <= result): # requried only screen
+            print("save image")
+        # cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+
+
     cv2.imshow('screen', img)
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
